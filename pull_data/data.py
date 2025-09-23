6da6918eb7122f2b757a37d29abcd596ad0a907b
At3ase <zhuoye0510@163.com>
2025-09-23 22:01:41 +0800

    pull_data/data.py

--- pull_data/data.py
+++ pull_data/data.py
@@ -3,7 +3,7 @@ from mp_api.client import MPRester
 # Option 1: Pass your API key directly as an argument.
 with MPRester("YOUR api") as mpr:
     # do stuff with mpr...
-    print(mpr.get_structure_by_material_id("mp-").formula)
+    print(mpr.get_structure_by_material_id("mp-4019").formula)
 # Option 2: Use the `MP_API_KEY` environment variable:
 # export MP_API_KEY="your_api_key_here"
 # Note: You can also configure your API key through pymatgen
