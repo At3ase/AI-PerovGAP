fce2bd39620bd664073cdc0eeb0cb499cdeaf9ff
At3ase <zhuoye0510@163.com>
2025-09-24 10:11:25 +0800

    change name of the api

--- pull_data/data.py
+++ pull_data/data.py
@@ -1,7 +1,7 @@
 from mp_api.client import MPRester
 
 # Option 1: Pass your API key directly as an argument.
-with MPRester("YOUR api") as mpr:
+with MPRester("MP_API_KEY") as mpr:
     # user stuff with mpr...
     print(mpr.get_structure_by_material_id("mp-4019").formula)
 # Option 2: Use the `MP_API_KEY` environment variable:
